f25437aa8da7edaf5e1012662c8b2b818cb87196
Thanh Hieu Nguyen <2401707N@student.gla.ac.uk>
2020-01-29 02:25:47 +0000

    Gun added
    Gun added that shoots only enemies, applies force.

---
+++ Assets/Gun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float gunForce;
+    public Camera fpsCam;
+
+    private Rigidbody hitObject;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        //makes a variable representing object hit
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
+        {
+            if (hit.transform.tag =="Enemy")
+            {
+                Debug.Log(hit.transform.name);
+                hitObject = hit.rigidbody;
+                hitObject.AddForce(fpsCam.transform.forward * gunForce);
+            }
+        }
+
+    }
+}
