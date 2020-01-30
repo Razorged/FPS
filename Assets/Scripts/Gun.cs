0346b41b44d46a69e9a8e8f0d5827c63e81a6ffb
Thanh Hieu Nguyen <2401707N@student.gla.ac.uk>
2020-01-30 00:49:57 +0000

    Main mechanic
    Main mechanic of game implemented, cant look at restricted layer objects.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,14 +7,18 @@ public class Gun : MonoBehaviour
     public float damage = 10f;
     public float gunForce;
     public Camera fpsCam;
+    public float fireRate = 15f;
+    public ParticleSystem muzzleFlash; 
 
     private Rigidbody hitObject;
-    
+
+    private float nextTimeToFire = 0f;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
 
@@ -23,6 +27,7 @@ public class Gun : MonoBehaviour
     {
         RaycastHit hit;
         //makes a variable representing object hit
+        muzzleFlash.Play();
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
         {
             if (hit.transform.tag =="Enemy")
