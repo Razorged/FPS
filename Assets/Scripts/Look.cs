fb44e31a86025f8590e6e14596d0136d397083c7
Thanh Hieu Nguyen <2401707N@student.gla.ac.uk>
2020-01-31 22:24:07 +0000

    Network beginning
    Made Player into prefab, made network manager and network identities

--- Assets/Scripts/Look.cs
+++ Assets/Scripts/Look.cs
@@ -7,6 +7,8 @@ public class Look : MonoBehaviour
     public float sensitivity;
     public Transform playerBody;
     public Camera playerCam;
+ 
+
     private float xRotation;
 
     
@@ -21,6 +23,8 @@ public class Look : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
         float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
@@ -31,4 +35,6 @@ public class Look : MonoBehaviour
         playerBody.Rotate(Vector3.up * mouseX);
 
     }
+    
+
 }
