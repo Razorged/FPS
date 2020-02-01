003708f8e57c4775c23e6b97064c45e2c6869685
Thanh Hieu Nguyen <2401707N@student.gla.ac.uk>
2020-02-01 00:27:34 +0000

    Multiplayer?

--- Assets/CreatePlayer.cs
+++ Assets/CreatePlayer.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class CreatePlayer : MonoBehaviour
+
+public class CreatePlayer : NetworkBehaviour
 {
+    public GameObject playerPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (isLocalPlayer == false)
+        {
+            return;
+        }
+        Debug.Log("Spawning in...");
+        CmdSpawnPlayer();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    //Methods/Commands
+    [Command]
+    void CmdSpawnPlayer()
+    {
+        GameObject go = Instantiate(playerPrefab);
+        NetworkServer.Spawn(go);
     }
 }
